6362dab60d95e0b59b9a98a7bb7791e9aab5189e
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2022-11-30 16:59:10 +0000

    change lmbda func name + add func dependency on log group

--- cloudformation/src/lambda.py
+++ cloudformation/src/lambda.py
@@ -69,13 +69,6 @@ log_retention_period = t.add_parameter(Parameter(
     Type="Number"
 ))
 
-function_name = t.add_parameter(Parameter(
-    "LambdaFunctionName",
-    Description="The lambda function name",
-    Default="chaos-lambda",
-    Type="String"
-))
-
 termination_topic = t.add_resource(
     Topic("ChaosLambdaTerminationTopic")
 )
@@ -131,16 +124,16 @@ lambda_role = Role(
 )
 t.add_resource(lambda_role)
 
-t.add_resource(LogGroup(
+lambda_log_group = t.add_resource(LogGroup(
     "ChaosLambdaLogGroup",
-    LogGroupName=Sub("/aws/lambda/${FunctionName}", FunctionName=Ref(function_name)),
+    LogGroupName=Sub("/aws/lambda/${AWS::StackName}-function"),
     RetentionInDays=Ref(log_retention_period),
 ))
 
 lambda_function = t.add_resource(Function(
     "ChaosLambdaFunction",
     Description="CloudFormation Lambda",
-    FunctionName=Ref(function_name),
+    FunctionName=Sub("${AWS::StackName}-function"),
     Code=lambda_code,
     Environment=Environment(Variables={
         "probability": Ref(default_probability),
@@ -152,6 +145,7 @@ lambda_function = t.add_resource(Function(
     Role=GetAtt(lambda_role, "Arn"),
     Runtime="python3.8",
     Timeout=30,
+    DependsOn=lambda_log_group.title
 ))
 
 chaos_lambda_rule = t.add_resource(Rule(
